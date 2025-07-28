81a90eadb29b36d538725d84b9124288ad0c887e
dhsoftHwang <bigtow@dhsoft.co.kr>
2025-07-28 10:25:15 +0900

    1차 수정(ajax 테스트 코드들 리팩토링, 아직 일부 코드 미적용)

--- teeth-Dentistry/src/main/java/teeth/Dentistry/portlet/TeethDentistryPortlet.java
+++ teeth-Dentistry/src/main/java/teeth/Dentistry/portlet/TeethDentistryPortlet.java
@@ -1,10 +1,19 @@
 package teeth.Dentistry.portlet;
 
 import teeth.Dentistry.constants.TeethDentistryPortletKeys;
+import teeth.model.TreatmentHistory;
+import teeth.service.TreatmentHistoryLocalServiceUtil;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import java.util.List;
+
 import javax.portlet.Portlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -19,7 +28,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=TeethDentistry",
 		"javax.portlet.init-param.template-path=/",
-		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.init-param.view-template=/teeth/view/teethview.jsp",
 		"javax.portlet.name=" + TeethDentistryPortletKeys.TEETHDENTISTRY,
 		"javax.portlet.resource-bundle=content.Language",
 		"javax.portlet.security-role-ref=power-user,user"
@@ -27,4 +36,40 @@ import org.osgi.service.component.annotations.Component;
 	service = Portlet.class
 )
 public class TeethDentistryPortlet extends MVCPortlet {
+	@Override
+    public void render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException {
+		try 
+		{
+			long PatientID = 1001; 
+			boolean isPermanent = false;
+			isPermanent = processTeethRange(renderRequest, PatientID, 11, 48, true, isPermanent);
+			isPermanent = processTeethRange(renderRequest, PatientID, 51, 85, false, isPermanent);
+			
+			renderRequest.setAttribute("patientID", PatientID);
+			renderRequest.setAttribute("isPermanent", isPermanent);
+			
+			super.render(renderRequest, renderResponse);
+		}
+		catch(Exception e)
+		{
+			_log.info("Error During Render MainPage!");
+			e.printStackTrace();
+		}
+	}
+	
+	private boolean processTeethRange(RenderRequest Request, long PatientID, long from, long to, boolean checkPermanent, boolean isPermanent)
+	{
+		for(long i = from; i <= to; i++)
+		{
+			List<TreatmentHistory> HT = TreatmentHistoryLocalServiceUtil.getPatientTreatmentListByTeethNum(PatientID, i);
+			if (checkPermanent && !HT.isEmpty()) 
+			{
+				isPermanent = true;
+			}
+			Request.setAttribute("teeth" + i, HT);
+	        //_log.info("teeth" + i + " : " + HT);
+		}
+		return isPermanent;
+	}
+	Log _log = LogFactoryUtil.getLog(TeethDentistryPortlet.class);
 }
